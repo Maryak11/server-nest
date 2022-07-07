6b2bfac23543900a56c3f576a312971cd0ed5976
Kirill Guryev <gurev.matvei@gmail.com>
2022-07-07 22:59:40 +0300

    feat: initil commit

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,31 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from 'src/users/users.service';
+import { AuthDto } from './dto/auth.dto';
+import * as bcrypt from 'bcryptjs';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private userService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async login(dto: AuthDto) {}
+
+  async registration(dto: AuthDto) {
+    const condidate = await this.userService.getUserByEmail(dto.email);
+    if (condidate) {
+      throw new HttpException(
+        'Пользователь с таким email уже существует',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+    const hashPassword = await bcrypt.hash(dto.password, 5);
+    const user = await this.userService.createUser({
+      ...dto,
+      password: hashPassword,
+    });
+    return;
+  }
+}
