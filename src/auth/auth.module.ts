194e3b88cf626fcad372f978717bae24a8cd0a17
Kirill Guryev <gurev.matvei@gmail.com>
2022-07-10 23:45:43 +0300

    feat: add jwt auth guard and role guard

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 
 import { UsersModule } from 'src/users/users.module';
@@ -9,13 +9,14 @@ import { AuthService } from './auth.service';
   controllers: [AuthController],
   providers: [AuthService],
   imports: [
-    UsersModule,
     JwtModule.register({
       secret: process.env.PRIVATE_KEY || 'SECRET',
       signOptions: {
         expiresIn: '24h',
       },
     }),
+    forwardRef(() => UsersModule),
   ],
+  exports: [AuthService, JwtModule],
 })
 export class AuthModule {}
